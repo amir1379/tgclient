bab2ff8f7aaf2bb25ae5a2b5fd2dd29aaecae90d
amir1379 <amir1379.110@gmail.com>
2018-12-09 18:18:40 +0330

    update

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 import setuptools
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name="tgclient",
-    packages=['tgclient'],
+    packages=setuptools.find_packages(),
     version='1.5.2',
     description='Telegram Bot Api Client',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='amirhossein faridvand',
     author_email='amirfaridvand@gmail.com',
     url='https://github.com/amir1379/tgclient',
